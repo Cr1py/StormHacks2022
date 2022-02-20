f4460741c1ba3c6634a154a5381cc2aa0f254c0f
Cr1py <92011184+Cr1py@users.noreply.github.com>
2022-02-20 02:42:44 -0800

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3VCqoUqK1drTIAXJqpP78beMkjbnb1MI",
-  authDomain: "stormhacks2022-341900.firebaseapp.com",
-  projectId: "stormhacks2022-341900",
-  storageBucket: "stormhacks2022-341900.appspot.com",
-  messagingSenderId: "902373617398",
-  appId: "1:902373617398:web:9ffa2175febee2a97846d7",
-  measurementId: "G-RD92PB4C1Y"
+// add your api json file data here
 };
 
 // Initialize Firebase
