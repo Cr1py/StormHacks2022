2efafa207cd2e42d74aec05ff58c16694899c44e
Cr1py <92011184+Cr1py@users.noreply.github.com>
2022-02-20 02:42:05 -0800

    Add files via upload

---
+++ src/pages/MyNotes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MyNotes() {
+    
+    return (
+        <div>
+            <h1>mynotes</h1>
+            <NotesGrid />
+        </div>
+
+    )
+}
+
+export default MyNotes
